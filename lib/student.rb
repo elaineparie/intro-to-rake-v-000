729a695fe77f36c8c1bdbd313847a8141cf3b477
Elaine Anderson <eparie@bu.edu>
2018-07-04 00:38:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,11 +52,5 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
-    Student.create_table
-  end
-  end
 
 end
